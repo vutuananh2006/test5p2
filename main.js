308e938c85331508cbd6d34d3f2aeb778e23c216
vutuananh2006 <vutuananh11102006@gmail.com>
2023-07-23 10:55:59 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+const person = {
+    name: 'Phước',
+    age: 17,
+    job:"student",
+};
+console.log(person.name);
+
+localStorage.setItem('user', JSON.stringify(person));
+console.log(setItem);
+
+localStorage.removeItem('name');
+console.log(removeItem);
+
+localStorage.setItem("name", "Hà");
+console.log(setItem);
+    
+
+
+
+const person = {
+    name: 'Phước',
+    age: 17,
+    job:"student",
+};
+console.log(person);
+
+localStorage.setItem('user', JSON.stringify(person));
+console.log(setItem);
+
+localStorage.removeItem('name');
+console.log(removeItem);
+
+
+
+
+
+const person = {
+    name: 'Phước',
+    age: 17,
+    job:"student",
+};
+console.log(person);
+
+localStorage.setItem('user', JSON.stringify(person));
+console.log(setItem);
+
+localStorage.clear();
+console.log(clear);
+
+
+
+
+
+
+var interval_obj = setInterval(function(){
+    alert("Thân em vừa trắng lại vừa tròn");
+    clearInterval(interval_obj);
+}, 1000);
+console.log(interval_obj);
+
+var interval_obj = setInterval(function(){
+    alert("Bảy nổi ba chìm với nước non");
+    clearInterval(interval_obj);
+}, 1000);
+console.log(interval_obj);
+
+var interval_obj = setInterval(function(){
+    alert("Rắn nát mặc dầu tay kẻ nặng");
+    clearInterval(interval_obj);
+}, 1000);
+console.log(interval_obj);
+
+var interval_obj = setInterval(function(){
+    alert("Mà em vẫn giữ tấm lòng son");
+    clearInterval(interval_obj);
+}, 1000);
+console.log(interval_ );
+
+
+
+
+    
+
+
+
+
+
+
